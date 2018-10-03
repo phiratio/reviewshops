f9ed7d70870833a43e418e1ab0a719fd945b901f
phiratio <phiratio161@gmail.com>
2018-10-03 23:49:26 +0300

    03.10.18 /top stores route fix, webpack npm run improvement

--- start.js
+++ start.js
@@ -27,9 +27,8 @@ require('./models/Review');
 // Start our app!
 const app = require('./app');
 app.set('port', process.env.PORT || 7777);
-const server = app.listen(app.get('port'), () => {
-//const server = app.listen(app.get('port'), 'localhost' () => {  <<< this is for digitalOcean
-    console.log(`${helpers.moment().format('DD/MM/YYYY||hh:mm:ss')} Express running → ${server.address()} on PORT ${server.address().port}`);
+const server = app.listen(app.get('port'), 'localhost', () => {
+    console.log(`${helpers.moment().format('DD/MM/YYYY||hh:mm:ss')} Express running → ${JSON.stringify(server.address())} on PORT ${server.address().port}`);
 });
 
 
